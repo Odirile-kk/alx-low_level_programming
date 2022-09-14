ffebbc56b87992dce47947479c49fa0cfc7780a6
Odirile-kk <kekanaodirile@gmail.com>
2022-09-14 03:02:26 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
-
 /**
  * print_sign - determin sign of int
- *
+ * 
  * @n: number to check for sign
  *
  * Return: 1 if positive, 0 if 0, and -1 if negative
- **/
+ */
 
 int print_sign(int n)
 {
@@ -16,14 +15,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
+	else
+	{
 	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	else
-	{
 		_putchar('-');
-		return (-1);
+		return (-1);
 	}
 }
