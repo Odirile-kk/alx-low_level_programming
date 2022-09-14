d6ae28598fb49561293afb8fffd7d3346735ed24
Odirile-kk <kekanaodirile@gmail.com>
2022-09-14 02:54:34 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,26 +1,17 @@
 #include "main.h"
 
-
 /**
- * _isalpha - returns 1 if c is a letter
+ * main - check the code
  *
- * @c: character to be checked if alpha
- *
- * Return: 1 if letter, 0 otherwise
- **/
+ * Return: Always 0.
+ */
 
 int _isalpha(int c)
 {
 	if ((c <= 90) && (c >= 65))
-	{
 		return (1);
-	}
 	else if ((c >= 97) && (c <= 122))
-	{
 		return (1);
-	}
-	else 
-	{
-		return (0);
-	}
+	else
+		return (0);
 }
