57ae572bd42d7fb5d76894906488bb6c8b025c13
Odirile-kk <kekanaodirile@gmail.com>
2022-09-15 11:27:22 -0700

    task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,18 +1,23 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
-int _isupper(int c)
-int _isdigit(int c)
-int mul(int a, int b)
-void print_numbers(void)
-void print_most_numbers(void)
-void more_numbers(void)
-void print_line(int n)
-void print_diagonal(int n)
-void print_square(int size)
-int main(void)
-int main(void)
-void print_triangle(int size)
-int exponent(int x, int y)
+/**
+* main - header file for prototypes
+* void protoypes (void)
+* int protoypes (int)
+*/
 
-#endif
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+#endif /* MAIN_H */ 
