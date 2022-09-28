2453d28fb926ac104ca882b4ca9f5f0469d3bfed
Odirile-kk <kekanaodirile@gmail.com>
2022-09-28 12:00:25 -0700

    mmm

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,13 @@
-int _putchar(char c);                                                                      
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* file: main.h
+* desc: this is the header file containing prototypes for all functions
+*
+*/
+
+int _putchar(char c);
 void _puts_recursion(char *s);                                                            
 void _print_rev_recursion(char *s);                                                       
 int _strlen_recursion(char *s);                                                           
@@ -8,3 +17,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);                                                               
 int is_palindrome(char *s);                                                               
 int wildcmp(char *s1, char *s2);
+
+#endif
